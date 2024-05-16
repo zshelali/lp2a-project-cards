6e94af09e7b6725b6a57386f852fd66d63e7c753
PierreGuerout <117822236+PierreGuerout@users.noreply.github.com>
2024-05-16 11:14:15 +0200

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -6,7 +6,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.print.attribute.standard.JobKOctetsSupported;
+//import javax.print.attribute.standard.JobKOctetsSupported;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -145,6 +145,8 @@ public class GUI extends JFrame implements ActionListener {
 			buttonPioche.setVisible(false);
 			buttonRank.setVisible(false);
 			buttonSuit.setVisible(false);
+			textScore.setBounds(300, 200, 400, 100);
+			textScore.setText(" Game Over       Your score is" + GameController.getScore() + "points");
 		}
 		else{
 		//desk updating
