73f4afcc1f8d33a6c7da0411fa1a447b8a336699
zshelali <ali.hajeri24@gmail.com>
2024-05-16 11:32:00 +0200

    updated CardDeck.java

--- src/GUI.java
+++ src/GUI.java
@@ -6,7 +6,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.print.attribute.standard.JobKOctetsSupported;
+//import javax.print.attribute.standard.JobKOctetsSupported;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
